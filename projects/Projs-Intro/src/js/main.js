9c968eb3e41c40c19324196aa5c645900662d654
李瑛 <liying@liyingdeMacBook-Pro.local>
2018-05-26 16:24:05 +0800

    remove projects

--- projects/Projs-Intro/src/js/main.js
+++
@@ -1,8 +0,0 @@
-define(['jquery', 'lunbo'], function(jq, lb){
-	var $ = jq;
-	lb.playLunBo($('.lunbo .imgLi'), 
-		$('.lunbo .imgLi li'), 
-		$('.lunbo .next'), 
-		$('.lunbo .prev'), 
-		$('.lunbo .bullet li'), true);
-})
